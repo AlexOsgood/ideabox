71b00d6d0c28dc5c3d35b6c965ee7931a8b5a623
Alex Osgood <alex.osgood@gmail.com>
2022-07-29 16:15:00 -0600

    Created data model

---
+++ main.js
@@ -0,0 +1,13 @@
+var filterStarIdeasText = document.querySelector("#filteredStarIdeas");
+var showStarredIdeasButton = document.querySelector("#showStarredIdeasButton");
+var titleInput = document.querySelector("#title-input");
+var bodyInput = document.querySelector("#body-input");
+var saveButton = document.querySelector("#save-button");
+var magnaGlassButton = document.querySelector("#magni-glass");
+var searchInput = document.querySelector("#search-input");
+var starButton = document.querySelector("#star-button");
+var commentButton = document.querySelector("#comment-icon");
+var deleteButton = document.querySelector("#delete-button");
+
+
+var savedIdeas = []
